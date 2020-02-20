5148a74add4fa88cf95173f4fa9aab3953942ea1
Nikitha Chittaluri <Nikitha_Chittaluri@epam.com>
2020-02-20 12:45:11 +0530

    Add project files.

---
+++ tokenizerexample/Controllers/DefaultController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace tokenizerexample.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
